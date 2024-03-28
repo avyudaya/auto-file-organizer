eb50c6f6509d44a007b11319b029ac12b77c653b
avyudaya <avyudaya7@gmail.com>
2024-03-28 21:53:20 +0545

    links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,14 +6,22 @@ import { FaInstagram } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 
 export default function Footer() {
+    const openInNewTab = (url) => {
+        window.open(url, "_blank", "noreferrer");
+    };
+
     return (
         <Flex mt={6} flexDir='column' align="center" justify="center" textAlign='center'>
             <Text>Made for the love of open source.</Text>
             <Flex mt={2}>
-                <IconButton isRound={true} onClick={() => { }} aria-label='LinkedIn' size='sm' color='gray.800' margin={0.5} icon={<FaLinkedin/>} />
-                <IconButton isRound={true} aria-label='Twitter' size='sm' color='gray.800' margin={0.5} icon={<FaXTwitter/>} />
-                <IconButton isRound={true} aria-label='Github' size='sm' color='gray.800' margin={0.5} icon={<FaGithub/>} />
-                <IconButton isRound={true} aria-label='Instagram' size='sm' color='gray.800' margin={0.5} icon={<FaInstagram/>} />
+                <IconButton onClick={() => openInNewTab('https://www.linkedin.com/in/avyudaya-acharya-510824169/')} aria-label='LinkedIn'
+                    isRound={true} size='sm' color='gray.800' margin={0.5} icon={<FaLinkedin />} />
+                <IconButton onClick={() => openInNewTab('https://twitter.com/Aavyudaya')} aria-label='Twitter'
+                    isRound={true} size='sm' color='gray.800' margin={0.5} icon={<FaXTwitter />} />
+                <IconButton onClick={() => openInNewTab('https://github.com/avyudaya/')} aria-label='Github'
+                    isRound={true} size='sm' color='gray.800' margin={0.5} icon={<FaGithub />} />
+                <IconButton onClick={() => openInNewTab('https://www.instagram.com/avyudaya.acharya/')} aria-label='Instagram'
+                    isRound={true} size='sm' color='gray.800' margin={0.5} icon={<FaInstagram />} />
             </Flex>
         </Flex>
     )
